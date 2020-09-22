72a3bbf4c137d56f14856b43823704d54395164b
MohammadFahad07 <fahad.mojumder@northsouth.edu>
2020-09-22 08:53:33 +0600

    1 added

---
+++ All Answer/1/1.asm
@@ -0,0 +1,46 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+.MODEL SMALL
+.STACK 100H
+.DATA
+
+CR EQU 0DH
+LF EQU 0AH
+
+MSG1 DB 'ENTER A LOWER CASE LETTER:  $'
+MSG2 DB 0DH,0AH, 'IN UPPER CASE ITS IS: '
+CHAR DB ?,'$'
+
+.CODE
+
+MAIN PROC
+    
+    MOV AX, @DATA       
+    MOV DS,AX           
+    
+    
+    LEA DX,MSG1         
+    MOV AH,9            
+    INT 21H           
+   
+    
+    MOV AH,1           
+    INT 21H             
+    SUB AL, 20H         
+    MOV CHAR, AL        
+    
+    
+    LEA DX,MSG2        
+    MOV AH,9            
+    INT 21H             
+    
+   
+    MOV AH,4CH
+    INT 21H             
+    
+MAIN ENDP
+    END MAIN
+
+
